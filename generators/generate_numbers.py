ff2a69507d1d669c1d521a82a173df29b65a7efe
gexi <gexi@yandex.ru>
2020-03-02 15:04:34 +0300

    Добавил генератор последовательности

---
+++ generators/generate_numbers.py
@@ -0,0 +1,21 @@
+def gen_bin(M, prefix=''):
+    if M == 0:
+        print(prefix)
+        return
+    for digit in "0", "1":
+        gen_bin(M - 1, prefix + digit)
+
+
+def generate_numbers(N: int, M: int, prefix=None):
+    prefix = prefix or []
+    if M == 0:
+        print(prefix)
+        return
+    for digit in range(N):
+        prefix.append(digit)
+        generate_numbers(N, M-1, prefix)
+        prefix.pop()
+
+
+#gen_bin(2)
+generate_numbers(4, 4)
